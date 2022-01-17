93bfcb4f709460907a7a8c02f6713691516f390a
newton mathias <newtymathias@gmail.com>
2022-01-17 13:28:15 +0300

    Update app.js

--- quesitions project/app.js
+++ quesitions project/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
-// traversing the dom
+/*const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e)=> {
+        const quesition = e.currentTarget.parentElement.parentElement;
+        quesition.classList.toggle('show-text');
+    })
+})
+*/
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', () => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove('show-text')
+            }  
+        })
+
+        question.classList.toggle('show-text')
+    })
+})
